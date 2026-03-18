24c2d030f38c663e296667284c4ae7e0394ff2a6
polakv93 <>
2026-03-18 17:47:07 +0100

    fix: better name

--- internal/tui/notify_windows.go
+++ internal/tui/notify_windows.go
@@ -3,9 +3,17 @@
 package tui
 
 import (
+	"sync"
+
 	"github.com/gen2brain/beeep"
 )
 
+var notificationAppNameOnce sync.Once
+
 func sendDesktopNotification(title, body string) error {
+	notificationAppNameOnce.Do(func() {
+		beeep.AppName = "azdo-tui"
+	})
+
 	return beeep.Notify(title, body, "")
 }
